aa616fc8aceddf98dab6fcff6d1ebafddf0b91f0
BettySosterics <betty.sosterics@gmail.com>
2021-11-28 16:34:41 +0100

    updated the search engine

--- src/index.js
+++ src/index.js
@@ -1,17 +1,3 @@
-function selectCity(event) {
-  event.preventDefault();
-  let input = document.querySelector("#city-input");
-  let city = document.querySelector("#current-city");
-  city.innerHTML = `${input.value}`;
-  let apiKey = "9d2606ef8c586cbebc46ab5a29408811";
-  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${input.value}&appid=${apiKey}&units=metric`;
-
-  axios.get(apiUrl).then(showTemp);
-}
-
-let form = document.querySelector("#city-form");
-form.addEventListener("submit", selectCity);
-
 function formatDate(timestamp) {
   let date = new Date(timestamp);
   let hours = date.getHours();
@@ -59,3 +45,17 @@ function showTemp(response) {
     `http://openweathermap.org/img/wn/${response.data.weather[0].description}@2x.png`
   );
 }
+
+function selectCity(event) {
+  event.preventDefault();
+  let input = document.querySelector("#city-input");
+  let city = document.querySelector("#current-city");
+  city.innerHTML = `${input.value}`;
+  let apiKey = "9d2606ef8c586cbebc46ab5a29408811";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${input.value}&appid=${apiKey}&units=metric`;
+
+  axios.get(apiUrl).then(showTemp);
+}
+
+let form = document.querySelector("#city-form");
+form.addEventListener("submit", selectCity);
