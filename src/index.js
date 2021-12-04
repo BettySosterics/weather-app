66da90d365f02b6bf5a99fba232d26b98683299a
BettySosterics <betty.sosterics@gmail.com>
2021-12-04 17:00:00 +0100

    display the forecast data in API

--- src/index.js
+++ src/index.js
@@ -21,29 +21,46 @@ function formatDate(timestamp) {
   return `${day} ${hours}:${minutes}`;
 }
 
+function formatDay(timestamp) {
+  let date = new Date(timestamp * 1000);
+  let day = date.getDay();
+  let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+  return days[day];
+}
+
 function displayForecast(response) {
-  console.log(response.data.daily);
+  let forecast = response.data.daily;
   let forecastElement = document.querySelector("#forecast");
 
   let forecastHTML = `<div class="row">`;
-  let days = ["Thu", "Fri", "Sat", "Sun", "Mon"];
-  days.forEach(function (day) {
-    forecastHTML =
-      forecastHTML +
-      `
+
+  forecast.forEach(function (forecastDay, index) {
+    if (index < 6) {
+      forecastHTML =
+        forecastHTML +
+        `
             <div class="col-2">
-              <div class="weather-forecast-date">${day}</div>
+              <div class="weather-forecast-date">${formatDay(
+                forecastDay.dt
+              )}</div>
               <img
-                src="http://openweathermap.org/img/wn/10d@2x.png"
+                src="http://openweathermap.org/img/wn/${
+                  forecastDay.weather[0].icon
+                }@2x.png"
                 alt=""
                 width="40"
               />
               <div class="weather-forecast-temperature">
-                <span class="weather-forecast-temperature-max">17° </span>
-                <span class="weather-forecast-temperature-min"> 15°</span>
+                <span class="weather-forecast-temperature-max">${Math.round(
+                  forecastDay.temp.max
+                )}° </span>
+                <span class="weather-forecast-temperature-min"> ${Math.round(
+                  forecastDay.temp.min
+                )}°</span>
               </div>
             </div>
         `;
+    }
   });
 
   forecastHTML = forecastHTML + `</div>`;
@@ -73,7 +90,7 @@ function showTemp(response) {
   celsiusTemperature = response.data.main.temp;
 
   cityElement.innerHTML = cityName;
-  tempElement.innerHTML = `${currentTemp}°C`;
+  tempElement.innerHTML = `${currentTemp}`;
   description.innerHTML = response.data.weather[0].description;
   humidity.innerHTML = response.data.main.humidity;
   windElement.innerHTML = Math.round(response.data.wind.speed);
